2f9d77bb06a6643354135b31a7e11dae0eb27dba
Alfie-F <alfie.fenables99Gmail.com>
2024-05-02 18:31:05 +0100

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,13 +16,12 @@ const Home = ({ items }) => {
               <li key={item.item_id} id="second">
                 Category: {item.category_name}
               </li>
-              <li>
+              <li id="pic">
                 <img
                   src="https://images.unsplash.com/photo-1519612632649-158aa883572a?fit=crop&w=600&h=600&q=80"
                   alt="picture of item"
-                  id="pic"
                   width="40px"
-                  height="60px"
+                  height="100px"
                 />
               </li>
               <li key={item.item_id} id="third">
